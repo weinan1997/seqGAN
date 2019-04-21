10d1a13f94a7fb85c3706a35eeab93846a2bcecf
weinan1997 <nathaniel_wei@outlook.com>
2019-04-21 12:53:55 +0800

    Finish text generating.

--- steg.py
+++ steg.py
@@ -8,7 +8,7 @@ def gen_text(gen, num, i2w):
         for word_idx in sample:
             if word_idx == 1:
                 break
-            text.append(i2w[word_idx])
+            text.append(i2w[word_idx.item()])
         texts.append(text)
     with open('image_coco_gen.txt', 'w') as f:
         for text in texts:
@@ -16,6 +16,7 @@ def gen_text(gen, num, i2w):
             for word in text:
                 temp += word
                 temp += ' '
+            temp += '\n'
             f.write(temp)
 
 gen = torch.load('gen.model')
